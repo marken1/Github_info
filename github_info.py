e0b842bb2a5e43ca2e0390a08471e603dc05cd74
Marek Šteinc <marek@steinc.cz>
2021-05-24 22:27:59 +0200

    Add files via upload

---
+++ github_info.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+def cont(url):
+    url_profile = 'https://github.com/marken1'
+    profile = requests.get(url_profile)
+    soup = BeautifulSoup(profile.content, 'html.parser')
+    find_el = soup.find('h2', class_='f4 text-normal mb-2')
+    con = re.findall(r'\d+', str(find_el.text))
+    return con
+
+
+def repo(url):
+    url_profile = 'https://github.com/marken1'
+    profile = requests.get(url_profile)
+    soup = BeautifulSoup(profile.content, 'html.parser')
+    rep = soup.find('span', class_='Counter')
+    return rep.text
+
+
+def stars(url):
+    profile = requests.get(url)
+    soup = BeautifulSoup(profile.content, 'html.parser')
+    stars = soup.find('span', class_='text-bold color-text-primary')
+    return stars.text
